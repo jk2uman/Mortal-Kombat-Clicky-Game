45bc37674ded5c218284807d3e6b15d8a86057d0
Julian Hicks <jkballin@gmail.com>
2019-06-12 18:22:02 -0700

    added music

--- src/component/Routine/Routine.js
+++ src/component/Routine/Routine.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Routine.css";
 
-const Routine = props => <div className="routine">{props.children}</div>;
+const Routine = props => <div className="routine">
+    <div className="music">
+        <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/81451959&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+    </div>
+    {props.children}</div>;
 
 export default Routine;
